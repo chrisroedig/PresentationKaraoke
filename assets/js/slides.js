5dbc9b2104058daf9515d9d11bd082ac867872a2
Christoph A. Roedig <croedig@covermymeds.com>
2018-09-17 14:54:53 -0400

    autoplay mode! and maybe fixed image url

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,13 +1,102 @@
 $(document).ready(function(){
-  show_random_slide()
+  build_rundown()
+  show_slide(window.current_slide_index)
+  window.show_playing = false
+  if(window.location.hash == '#autoplay'){
+    start_show()
+  }
 })
-$(document).keyup(function(){
-  show_random_slide()
+
+$(document).keyup(function(e){
+  switch(parseInt(e.which)) {
+    case 39:
+      next_slide();
+    break;
+    case 37:
+      previous_slide();
+    break;
+    case 27:
+      exit_show();
+    break;
+    case 66:
+      toggle_black();
+    break;
+    case 80:
+      pause_show();
+    break;
+    default:
+      console.log(e.which)
+    break;
+
+  }
 })
-var show_random_slide = function (){
+
+var start_show = function(){
+  console.log('starting show...')
+  window.show_playing = true;
+  next_slide_timed()
+}
+
+var pause_show = function(){
+
+  if(window.show_playing == false){
+    start_show()
+  }else{
+    console.log('pausing show...')
+    window.show_playing = false;
+  }
+}
+
+var next_slide_timed = function(){
+  setTimeout(function(){
+    if(window.show_playing == false){
+      return;
+    }
+    console.log('showing next slide')
+    next_slide();
+    next_slide_timed();
+  },10000)
+}
+
+var build_rundown = function(){
+  window.slides = _.shuffle($('.slide'))
+  window.current_slide_index = 0
+  window.isblack = false
+  window.last_slide_index = window.slides.length-1
+}
+
+var show_slide = function(index){
+  if(window.isblack == true){
+    return;
+  }
   $('.slide').hide()
-  var slides = $('.slide')
-  var slide_count = slides.length
-  var slide_index = Math.round(Math.random()*(slide_count-1))
-  $(slides[slide_index]).show()
+  $(slides[index]).show()
+  window.current_slide_index = index
+}
+
+next_slide = function (){
+  if(window.current_slide_index==window.last_slide_index){
+    return;
+  }
+  show_slide(window.current_slide_index+1)
+}
+
+previous_slide = function (){
+  if(window.current_slide_index==0){
+    return;
+  }
+  show_slide(window.current_slide_index-1)
+}
+
+exit_show = function (){
+  window.location.href = window.site_url
+}
+
+toggle_black = function (){
+  if(window.isblack == true){
+      $('.slides').removeClass('presentation-black')
+  }else{
+    $('.slides').addClass('presentation-black')
+  }
+  window.isblack = !window.isblack
 }
